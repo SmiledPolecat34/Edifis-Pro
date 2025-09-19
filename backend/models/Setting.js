ee564a73c2bc5fb1c6e8d592e97f15d48ef36bc9
SmiledPolecat34 <versayo03@gmail.com>
2025-09-19 22:52:59 +0200

    chore(setting): refactor model definition for better readability

--- backend/models/Setting.js
+++ backend/models/Setting.js
@@ -1,19 +1,23 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Setting = sequelize.define('Setting', {
-  key: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-    allowNull: false,
+const Setting = sequelize.define(
+  'Setting',
+  {
+    key: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  value: {
-    type: DataTypes.STRING,
-    allowNull: false,
+  {
+    tableName: 'settings',
+    timestamps: false,
   },
-}, {
-  tableName: 'settings',
-  timestamps: true,
-});
+);
 
 module.exports = Setting;
