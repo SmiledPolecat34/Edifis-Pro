67b0263523e6bbe09425ebcd9c70a93491f5fe97
SmiledPolecat34 <versayo03@gmail.com>
2025-09-23 16:20:57 +0200

    style(footer): remove unnecessary height property for footer

--- frontend/edifis-pro/src/layout/PageLayout.tsx
+++ frontend/edifis-pro/src/layout/PageLayout.tsx
@@ -7,32 +7,31 @@ import Footer from './footer/Footer';
 import ScrollButton from '../components/scrollButton/ScrollButton'; // Import ScrollButton
 
 const PageLayout = () => {
-    const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-    const scrollableContainerRef = useRef<HTMLDivElement>(null); // Create a ref for the scrollable container
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const scrollableContainerRef = useRef<HTMLDivElement>(null);
 
-    return (
-        <div className="flex h-screen bg-gray-100">
-            <SideBar isMobileNavOpen={isMobileNavOpen} setIsMobileNavOpen={setIsMobileNavOpen} />
-            <div 
-                ref={scrollableContainerRef} // Attach the ref
-                className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden md:ms-[250px]"
-            >
-                <Header setIsMobileNavOpen={setIsMobileNavOpen} />
-                <main className="flex-1">
-                    <Outlet />
-                </main>
-                <Footer />
-                {/* Overlay for mobile nav */}
-                {isMobileNavOpen && (
-                    <div 
-                        className="fixed inset-0 bg-black bg-opacity-50 z-30 md:hidden"
-                        onClick={() => setIsMobileNavOpen(false)}
-                    ></div>
-                )}
-                <ScrollButton scrollableRef={scrollableContainerRef} /> {/* Pass the ref as a prop */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <SideBar isMobileNavOpen={isMobileNavOpen} setIsMobileNavOpen={setIsMobileNavOpen} />
+      <div
+        ref={scrollableContainerRef}
+        className="relative flex flex-1 flex-col md:ms-[250px] min-h-screen"
+      >
+        <Header setIsMobileNavOpen={setIsMobileNavOpen} />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer /> {/* colle toujours en bas */}
+        {isMobileNavOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-30 md:hidden"
+            onClick={() => setIsMobileNavOpen(false)}
+          ></div>
+        )}
+        <ScrollButton scrollableRef={scrollableContainerRef} />
+      </div>
+    </div>
+  );
 };
 
 export default PageLayout;
